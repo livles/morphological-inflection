000602519605182171a744b823bd96c776094d42
Lucy Yang <ge87wod2@login-04.ai.lrz.de>
2025-09-30 14:03:21 +0200

    todo: test file locally

--- preprocessing_to_json/preprocessing_to_json.py
+++ preprocessing_to_json/preprocessing_to_json.py
@@ -1,5 +1,7 @@
 TRAIN_FILEPATH = ""
 TEST_FILEPATH = ""
+OUTPUT_TRAIN_FILEPATH = ""
+OUTPUT_TEST_FILEPATH = ""  
 LANGS = []
 
 
@@ -7,12 +9,15 @@ def preprocess(x):
     lemma, features, target = x.split("\t")
     prompt = "inflect " + lemma + " using " + features
     target_string = lemma+"\t"+features+"\t"+target
-    
+    line = '{"text":"'+prompt+'","summary":"'+target_string+'"}'
 
 
 for lang in LANGS:
-    with open(TRAIN_FILEPATH,"r") as train_file, open(TEST_FILEPATH,"r") as test_file:
+    with open(TRAIN_FILEPATH,"r") as train_file, open(TEST_FILEPATH,"r") as test_file, open (OUTPUT_TRAIN_FILEPATH + "_"+lang+ ".json","w") out_train_file, open (OUTPUT_TEST_FILEPATH + "_" + lang + ".json", "w") as out_test_file:
         train_lines = train_file.readlines()
         test_lines = test_file.readlines()
-        train_prompts = map(f,train_lines)
-        test_prompts = map(f,test_lines)
+        train_lines_list = map(f,train_lines)
+        test_json_lines_list = map(f,test_lines)
+        train_json_lines = "\n".join(train_lines_list)
+        test_json_lines = "\n".join(test_json_lines_list)
+
