914b5a22b7f63ffd74becf00e628ebf3d7dd8b9f
Lucy Yang <ge87wod2@login-03.ai.lrz.de>
2025-10-26 12:57:14 +0100

    tested mul with language code and language word

--- ByT5/sbatch_script.sh
+++ ByT5/sbatch_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --partition lrz-hgx-h100-94x4
-#SBATCH --gres gpu:4
-#SBATCH --time 2-00:00:00
-#SBATCH --output %j.out
+#SBATCH --partition lrz-hgx-a100-80x4
+#SBATCH --gres gpu:1
+#SBATCH --time 12:00:00
+#SBATCH --output lrz-hgx-a100-80x4.out
 
 python3 fine_tuned_ByT5.py
 
